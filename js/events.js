88e1c675d30b7a0a3acad5129fb655e7cf11ed41
Aude Faucheux <aude53@gmail.com>
2019-07-12 20:32:12 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 }
 
 function pressIt() {
-  $('document').on('keydown', function(e) {
+  $('document').on('keydown', function(key) {
     if(key.which === 103) {
       return alert('You pressed g');
     }
