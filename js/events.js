eae87af2b14cedc6ea8641896e622263a146d566
Aude Faucheux <aude53@gmail.com>
2019-07-12 20:11:55 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
+function getIt() {
+  return $('p').on('click', function() {
+
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
+  return getIt();
 
 });
